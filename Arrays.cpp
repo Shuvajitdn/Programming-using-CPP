563ecf4c671cd116bdb7b8a0592aba4ea1801844
Shuvajit Debnath <118671478+Shuvajitdn@users.noreply.github.com>
2024-06-07 17:37:45 +0530

    Add files via upload

---
+++ Arrays.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, c, d, e;
+
+    cout << "Give 5 Integers as Input" << endl;
+    cin>> a>>b>>c>>d>>e;
+
+    cout<<"The integers in reverse order are : " <<e<<d<<c<<b<<a;
+
+    return 0;
+}
